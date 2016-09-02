92aa0c614fdae69cea7e7270fbed998410312a49
Devin <qingyong@linghit.com>
2016-09-02 11:16:14 +0800

    只对打包的做判断

--- plugin/src/main/groovy/com/yong/plugin/BuildFileMovePlugin.groovy
+++ plugin/src/main/groovy/com/yong/plugin/BuildFileMovePlugin.groovy
@@ -12,6 +12,25 @@ public class BuildFileMovePlugin implements Plugin<Project> {
 
     @Override
     void apply(Project project) {
+
+        // 只有传递到Jenkins的平台上面的project才需要进行下面的操作
+        // 这里通过判断gradle.properties文件里面的一些属性来确定是否是
+        // 在Jenkins上面打包的
+        File proFile = new File(project.rootDir, "gradle.properties");
+
+        // 没有的话
+        if (!proFile.exists()) return;
+
+        Properties properties = new Properties();
+        properties.load(proFile.newDataInputStream());
+        def name = properties.getProperty("MAVEN_USERNAME");
+        def pass = properties.getProperty("MAVEN_PASSWORD");
+
+        // 不是打包的不执行下面的操作
+        if (name == null || name.trim().length() <= 0 || !name.equals("")
+                || pass == null || pass.trim().length() <= 0 || !pass.equals("")
+        ) return;
+
         // app禁止debug的task，lib禁止release的task
         disableTestTask(project);
         if (project.plugins.hasPlugin(AppPlugin)) {
